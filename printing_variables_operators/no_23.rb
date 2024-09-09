881356603b6d2eee7138dc34e367cb8791a75b8f
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-09-10 07:46:26 +0800

    No.23: Created 2 string variables, outputs a sentence about the book

---
+++ printing_variables_operators/no_23.rb
@@ -0,0 +1,4 @@
+bookTitle = "Clean Code"
+author = "Robert C. Martin"
+
+puts "The book #{bookTitle} is written by #{author} and it is a great book for programmers to adapt a habit in producing a cleaner code."
