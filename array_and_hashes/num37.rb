a549d717c103450059dc3f10daaaed0a01cbcd52
jaushi <joshpagallaman10@gmail.com>
2024-09-10 19:14:50 +0800

    No.37: An array of hashes, prints the name of student 10

---
+++ array_and_hashes/num37.rb
@@ -0,0 +1,14 @@
+students = [
+  { name: "John", age: 15, grade: "A" },
+  { name: "Joshua", age: 16, grade: "B" },
+  { name: "Jake", age: 15, grade: "A" },
+  { name: "Blake", age: 17, grade: "C" },
+  { name: "Kyle", age: 16, grade: "B" },
+  { name: "Jude", age: 15, grade: "A" },
+  { name: "Wanda", age: 17, grade: "C" },
+  { name: "Bella", age: 16, grade: "B" },
+  { name: "Daphne", age: 15, grade: "A" },
+  { name: "bianca", age: 17, grade: "C" }
+]
+
+puts students[9][:name]
