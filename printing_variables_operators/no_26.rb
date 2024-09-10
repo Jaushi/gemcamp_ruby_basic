d655408fbdf03ad7031111086031a48d2e1389dc
Jaushi <144474840+Jaushi@users.noreply.github.com>
2024-09-10 08:02:58 +0800

    No.26: Concatenates the first 3 and last 2 characters of both strings

---
+++ printing_variables_operators/no_26.rb
@@ -0,0 +1,10 @@
+variable1 = "Banana"
+variable2 = "Lego"
+
+first_part = variable1[0, 3]
+
+second_part = variable2[-2, 2]
+
+result = first_part + second_part
+
+puts "The concatenated result is: #{result}"
