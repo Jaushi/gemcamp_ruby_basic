ff4cc8af49a098b9db86a71afba3ece936270cff
jaushi <joshpagallaman10@gmail.com>
2024-09-10 18:22:27 +0800

    No.23: Prints the author of the second book

---
+++ array_and_hashes/num23.rb
@@ -0,0 +1,7 @@
+favoriteBooks = {
+  "Clean code" => "Robert C. Martin",
+  "Diary of a Wimpy kid" => "Jeff Kinney",
+  "How to train your dragon" => "Cressida Cowell"
+}
+
+puts favoriteBooks["Diary of a Wimpy kid"]
