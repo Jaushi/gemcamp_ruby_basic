0e688a5ecf66a2f92996ff31ff18bf2954761de3
Jaushi <joshpagallaman10@gmail.com>
2024-09-11 15:20:55 +0800

    No.3: Checks if the numbers are equal

---
+++ conditional_statements/num3.rb
@@ -0,0 +1,11 @@
+puts "Enter number 1:"
+num1 = gets.chomp.to_i
+
+puts "Enter number 2:"
+num2 = gets.chomp.to_i
+
+if num1 == num2
+  puts "The numbers are equal"
+else
+  puts "The numbers are not equal"
+end
