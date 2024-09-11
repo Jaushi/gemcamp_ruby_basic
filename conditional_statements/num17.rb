d7a68b00380b48c2dbe7a51ba8e23a4ed890e776
Jaushi <joshpagallaman10@gmail.com>
2024-09-11 16:02:04 +0800

    No.17: Checks if the inputted words are identical

---
+++ conditional_statements/num17.rb
@@ -0,0 +1,11 @@
+puts "Enter a word 1:"
+word_1 = gets.chomp
+
+puts "Enter a word 2:"
+word_2 = gets.chomp
+
+if word_1 == word_2
+  puts "The words are identical"
+else
+  puts "The words are different"
+end
